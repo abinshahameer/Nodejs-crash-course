f813403b64df82258423d6b015bba8db68564fec
Abinshah Ameer <62852309+abinshahameer@users.noreply.github.com>
2020-07-18 12:52:38 +0530

    Update app.js
    mongo db url deleted

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const blogRoutes = require('./routes/blogRoutes');
 const app = express();
 
 //connect to mongodb
-const dbURI='mongodb+srv://netninja:test1234@cluster0.m9axy.mongodb.net/node?retryWrites=true&w=majority';
+const dbURI='<use mongodb url>';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
   .catch(err => console.log(err));
